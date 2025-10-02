e906f39ee32c56b53df54af66e243f0481e8d536
ATTHAPOL\Atthapol <Atthapol>
2025-10-02 10:30:30 +0700

    add

--- prisma/migrations/20250930025508_add_employee_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Employee` ADD COLUMN `birthDate` DATETIME(3) NULL,
-    ADD COLUMN `company` VARCHAR(191) NULL,
-    ADD COLUMN `gender` ENUM('MALE', 'FEMALE', 'OTHER') NULL,
-    ADD COLUMN `responsibilityArea` VARCHAR(191) NULL;
